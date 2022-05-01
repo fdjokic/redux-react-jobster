1967394e50cc1b027f7785f5ddbd6b8a50694bb0
Your Name <ffilip.djokic@gmail.com>
2022-05-01 16:59:00 +0200

    Stats complete

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,15 +1,29 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { ChartsContainer, StatsContainer } from "../../components";
+import Loading from "../../components/Loading";
 import { showStats } from "../../features/allJobs/allJobsSlice";
 
 const Stats = () => {
   const dispatch = useDispatch();
+  const { isLoading, monthlyApplication } = useSelector(
+    (store) => store.allJobs
+  );
 
   useEffect(() => {
     dispatch(showStats());
   }, []);
 
-  return <div>Stats</div>;
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <StatsContainer />
+      <ChartsContainer />
+    </>
+  );
 };
 
 export default Stats;
